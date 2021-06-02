5ae119e5ed402126773c875c3e4c7936dfeffd10
Michael Linderman <mlinderman@middlebury.edu>
2021-06-02 15:58:09 -0400

    Qualtiy driven allele channel

--- src/npsv2/images.py
+++ src/npsv2/images.py
@@ -109,7 +109,11 @@ class ImageGenerator:
         )
 
     def _allele_pixel(self, realignment: AlleleRealignment):
-        return self._allele_to_pixel[realignment.allele]
+        if realignment.ref_quality is None or realignment.alt_quality is None:
+            return 0
+        else:
+            return (realignment.alt_quality - realignment.ref_quality) / 40 * 100 + 150
+        #return self._allele_to_pixel[realignment.allele]
 
     def _strand_pixel(self, read: pysam.AlignedSegment):
         return self._strand_to_pixel[Strand.NEGATIVE if read.is_reverse else Strand.POSITIVE]
@@ -126,7 +130,7 @@ class ImageGenerator:
         # TODO: Combine all the channels into a single image, perhaps BASE, INSERT_SIZE, ALLELE (with
         # mapq as alpha)...
         channels = [ALIGNED_CHANNEL, REF_PAIRED_CHANNEL, ALLELE_CHANNEL]
-        channels = 3*[ALT_ALLELE_CHANNEL]
+        channels = 3*[ALLELE_CHANNEL]
         return Image.fromarray(image_tensor[:, :, channels], mode="RGB")    
 
 
@@ -267,7 +271,7 @@ class SingleHybridImageGenerator(SingleImageGenerator):
 
 class SingleDepthImageGenerator(SingleImageGenerator):
     def __init__(self, cfg):
-        super().__init__(cfg, num_channels=8) #7)
+        super().__init__(cfg, num_channels=7) #8)
         
 
     def _generate(self, variant, read_path, sample: Sample, regions, realigner, ref_seq: str = None, **kwargs):
@@ -309,12 +313,12 @@ class SingleDepthImageGenerator(SingleImageGenerator):
                 image_tensor[row_idxs[col_slice], col_idxs, MAPQ_CHANNEL] = self._qual_pixel(read.mapq, self._cfg.pileup.max_mapq)
                 image_tensor[row_idxs[col_slice], col_idxs, REF_PAIRED_CHANNEL] = self._zscore_pixel(read.ref_zscore)
                 image_tensor[row_idxs[col_slice], col_idxs, ALT_PAIRED_CHANNEL] = self._zscore_pixel(read.alt_zscore)
-                #image_tensor[row_idxs[col_slice], col_idxs, ALLELE_CHANNEL] = self._allele_pixel(read.allele)
+                image_tensor[row_idxs[col_slice], col_idxs, ALLELE_CHANNEL] = self._allele_pixel(read.allele)
                 image_tensor[row_idxs[col_slice], col_idxs, STRAND_CHANNEL] = self._strand_to_pixel[read.strand]
                 image_tensor[row_idxs[col_slice], col_idxs, BASEQ_CHANNEL] = self._qual_pixel(read.baseq(read_slice), self._cfg.pileup.max_baseq)
                 
-                image_tensor[row_idxs[col_slice], col_idxs, REF_ALLELE_CHANNEL] = self._qual_pixel(read.allele.ref_quality, self._cfg.pileup.max_alleleq)
-                image_tensor[row_idxs[col_slice], col_idxs, ALT_ALLELE_CHANNEL] = self._qual_pixel(read.allele.alt_quality, self._cfg.pileup.max_alleleq)
+                #image_tensor[row_idxs[col_slice], col_idxs, REF_ALLELE_CHANNEL] = self._qual_pixel(read.allele.ref_quality, self._cfg.pileup.max_alleleq)
+                #image_tensor[row_idxs[col_slice], col_idxs, ALT_ALLELE_CHANNEL] = self._qual_pixel(read.allele.alt_quality, self._cfg.pileup.max_alleleq)
 
                 # Increment the 'current' row for the bases we just added to the pileup, overwrite the last row if we exceed
                 # the maximum coverage
